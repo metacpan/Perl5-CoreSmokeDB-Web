581e27663b541276fe3935ba2a7d87a564f8e861
Abe Timmerman <abeltje@cpan.org>
2022-10-13 19:46:20 +0200

    Add query-params to the search page
    Some people had a list of bookmarked-searches with query params, this
    version of the search-page supports them.

--- src/components/P5SDBSearch.vue
+++ src/components/P5SDBSearch.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="content"> 
+  <div id="content">
     <h2>Search Perl5 CoreSmokeDB</h2>
 
     <div v-if="isFetching" class="spinner" />
@@ -39,6 +39,7 @@
 import { reactive }               from 'vue';
 import { mapGetters, mapActions } from 'vuex';
 import { logTrace, logDebug }     from '@/helpers/logging';
+import { hasLength }              from '@/helpers';
 import P5SDBSearchForm   from '@/components/P5SDBSearchForm.vue';
 import P5SDBReportsTable from '@/components/P5SDBReportsTable.vue';
 import P5SDBFullFooter   from '@/components/P5SDBFullFooter.vue';
@@ -49,6 +50,24 @@ export default {
     P5SDBSearchForm,
     P5SDBReportsTable,
     P5SDBFullFooter,
+  },
+  props: {
+    selected_arch:   {type: String, default: ""},
+    selected_host:   {type: String, default: ""},
+    selected_osnm:   {type: String, default: ""},
+    selected_osvs:   {type: String, default: ""},
+    selected_comp:   {type: String, default: ""},
+    selected_cver:   {type: String, default: ""},
+    selected_branch: {type: String, default: ""},
+    selected_perl:   {type: String, default: "latest"},
+    andnotsel_arch:  {type: String, default: ""},
+    andnotsel_host:  {type: String, default: ""},
+    andnotsel_osnm:  {type: String, default: ""},
+    andnotsel_osvs:  {type: String, default: ""},
+    andnotsel_comp:  {type: String, default: ""},
+    andnotsel_cver:  {type: String, default: ""},
+    page:            {type: Number, default: 1 },
+    rpp:             {type: Number, default: 25},
   },
   data() {
     return {
@@ -91,6 +110,36 @@ export default {
   mounted() {
     logTrace("searchParameters mounted");
     if (!this.hasSearchParameters) { this.getSearchParametersNow(); }
+
+    this.searchFormValues.selectedArch        = this.selected_arch;
+    this.searchFormValues.selectedHost        = this.selected_host;
+    this.searchFormValues.selectedOsname      = this.selected_osnm;
+    this.searchFormValues.selectedOsversion   = this.selected_osvs;
+    this.searchFormValues.selectedCc          = this.selected_comp;
+    this.searchFormValues.selectedCcversion   = this.selected_cver;
+    this.searchFormValues.selectedBranch      = this.selected_branch;
+    this.searchFormValues.selectedPerlVersion = this.selected_perl;
+    this.searchFormValues.andnotArch          = this.andnotsel_arch ? "not" : "and";
+    this.searchFormValues.andnotHost          = this.andnotsel_host ? "not" : "and";
+    this.searchFormValues.andnotOsname        = this.andnotsel_osnm ? "not" : "and";
+    this.searchFormValues.andnotOsversion     = this.andnotsel_osvs ? "not" : "and";
+    this.searchFormValues.andnotCc            = this.andnotsel_comp ? "not" : "and";
+    this.searchFormValues.andnotCcversion     = this.andnotsel_cver ? "not" : "and";
+    const autoSearch = hasLength(this.selected_arch)
+                    || hasLength(this.selected_host)
+                    || hasLength(this.selected_osnm)
+                    || hasLength(this.selected_osvs)
+                    || hasLength(this.selected_comp)
+                    || hasLength(this.selected_cver)
+                    || hasLength(this.selected_branch)
+                    || (this.selected_perl !== 'latest')
+                    || hasLength(this.andnotsel_arch)
+                    || hasLength(this.andnotsel_host)
+                    || hasLength(this.andnotsel_osnm)
+                    || hasLength(this.andnotsel_osvs)
+                    || hasLength(this.andnotsel_comp)
+                    || hasLength(this.andnotsel_cver);
+    if (autoSearch) { this.submitSearch() }
   },
   methods: {
     ...mapActions({
@@ -133,19 +182,20 @@ export default {
     currentSearchArguments() {
       const args = {
         selected_arch:   this.searchFormValues.selectedArch,
+        selected_host:   this.searchFormValues.selectedHost,
         selected_osnm:   this.searchFormValues.selectedOsname,
         selected_osvs:   this.searchFormValues.selectedOsversion,
-        selected_host:   this.searchFormValues.selectedHost,
         selected_comp:   this.searchFormValues.selectedCc,
         selected_cver:   this.searchFormValues.selectedCcversion,
         selected_perl:   this.searchFormValues.selectedPerlVersion,
         selected_branch: this.searchFormValues.selectedBranch,
         andnotsel_arch:  this.searchFormValues.andnotArch === 'not' ? 1 : 0,
+        andnotsel_host:  this.searchFormValues.andnotHost === 'not' ? 1 : 0,
         andnotsel_osnm:  this.searchFormValues.andnotOsname === 'not' ? 1 : 0,
         andnotsel_osvs:  this.searchFormValues.andnotOsversion === 'not' ? 1 : 0,
-        andnotsel_host:  this.searchFormValues.andnotHost === 'not' ? 1 : 0,
         andnotsel_comp:  this.searchFormValues.andnotCc === 'not' ? 1 : 0,
         andnotsel_cver:  this.searchFormValues.andnotCcversion === 'not' ? 1 : 0,
+        reports_per_page: this.rpp,
       };
       return args;
     },
@@ -181,7 +231,7 @@ export default {
     submitSearch() {
       const searchArgs = {
         ...this.currentSearchArguments(),
-        page: 1,
+        page: this.page,
       };
 
       logDebug(`submitSearch: ${JSON.stringify(searchArgs)}`);
